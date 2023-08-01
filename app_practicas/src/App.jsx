74d3458e84aee9e9e9430dffff6d3a9b7d4a5c43
Main <gimenezfrancotomas96@gmail.com>
2023-08-01 16:36:16 -0300

    creacion de app de practicas y app de loguin

---
+++ app_practicas/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App({saluda_gato}) {
+
+  return (
+    <>
+     <h1>Bienvenido {saluda_gato}</h1>
+    </>
+  )
+}
+
+export default App
