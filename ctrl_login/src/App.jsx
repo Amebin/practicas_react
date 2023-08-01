4d32a24c596f4b60b1b07ed5ebcbf992b97b4f84
Main <gimenezfrancotomas96@gmail.com>
2023-08-01 17:10:44 -0300

    prueba de props

--- ctrl_login/src/App.jsx
+++ ctrl_login/src/App.jsx
@@ -1,9 +1,16 @@
-
+import Mensaje from './Mensaje.jsx'
 import './App.css'
 
 function App() {
    return (
     <>
+    <div>
+    <h1>Control Login</h1>
+    </div>
+
+    <div>
+      <Mensaje msj='Este es el primer mensaje'/>
+    </div>
     </>
   )
 }
