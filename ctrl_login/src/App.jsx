aa9d756541dc30af0949186a40defe770994e30a
Main <gimenezfrancotomas96@gmail.com>
2023-08-02 21:38:20 -0300

    se añaden rutas de navegacion

--- ctrl_login/src/App.jsx
+++ ctrl_login/src/App.jsx
@@ -1,18 +1,26 @@
-import Mensaje from './Mensaje.jsx'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+
+import Menu from './components/Menu.jsx'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import GiftCards from './pages/GiftCards.jsx'
+import Error404 from './pages/Error404.jsx'
 
 function App() {
-   return (
-    <>
-    <div>
-    <h1>Control Login</h1>
-    </div>
+  return (
+    <Router>
 
-    <div>
-      <Mensaje msj='Este es el primer mensaje'/>
-    </div>
-    </>
-  )
+      <Menu />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/giftcards" element={<GiftCards />} />
+       
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
