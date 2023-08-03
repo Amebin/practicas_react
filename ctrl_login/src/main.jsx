51a6c638a88f3b542c2eada3aa0d013a2dde575e
Main <gimenezfrancotomas96@gmail.com>
2023-08-02 21:40:24 -0300

    se importan estilos de bootstrap e iconos de font awesome

--- ctrl_login/src/main.jsx
+++ ctrl_login/src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
